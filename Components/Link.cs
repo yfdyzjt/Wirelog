b6f093c8fd630da54ffa42b9325a8f23f96d1d7c
yfdyzjt <yfdyzjt@gmail.com>
2025-08-12 08:41:04 +0800

    Fix timer can't activated bug

---
+++ Components/Link.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wirelog
+{
+    public static class Link
+    {
+        public static bool Add(Wire wire, Gate gate)
+        {
+            return wire.Gates.Add(gate) && gate.OutputWires.Add(wire);
+        }
+    }
+}
